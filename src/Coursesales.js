0d7f2c355d96e22fbd0bb0311d33e6a8a5783f2d
Takaforyannick30 <takaforyannick30@gmail.com>
2021-04-19 23:06:02 +0200

    Created coursesales component

---
+++ src/Coursesales.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Coursesales extends Component {
+  render() {
+    return (
+      <div className="">
+          Course sales component
+        
+      </div>
+    );
+
+  }
+ 
+}
+
+export default Coursesales;
